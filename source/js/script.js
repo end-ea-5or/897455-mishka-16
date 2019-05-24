531b867ff905647f1e690a0a3d8f0dcd4f4bc2f9
end-ea-5or <marsel20002008@yandex.ru>
2019-05-24 19:52:39 +0300

    окончательные правки (но это не точно)

--- source/js/script.js
+++ source/js/script.js
@@ -2,10 +2,12 @@ var basketLink = document.querySelectorAll(".button__modal");
 var popup = document.querySelector(".modal");
 var bg = document.querySelector(".bg");
 var form = document.querySelector(".modal__form");
-var mapImg = document.querySelector(".contacts__map--img");
+var mapImg = document.querySelector(".contacts__map-img");
 var mapFrame = document.querySelector(".contacts__map");
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
 
-for (var i=0; i < basketLink.length; i++) {
+for (var i = 0; i < basketLink.length; i++) {
   var element = basketLink[i];
   element.addEventListener("click", function (evt) {
     evt.preventDefault();
@@ -41,6 +43,18 @@ window.addEventListener("keydown", function (evt) {
 });
 
 if (mapImg) {
-  mapImg.classList.add("contacts__map--close");
+  mapImg.classList.add("contacts__map-img--close");
   mapFrame.classList.add("contacts__map--open");
 }
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
